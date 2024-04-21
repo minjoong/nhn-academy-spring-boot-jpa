a188ce5e58341a5b9c9c4693c6fdfc0ebc1999a8
김민중 <minjoong.slow@gmail.com>
2024-04-21 21:47:58 +0900

    `order_item` 테이블에 대한 entity 매핑 실습 정답
    @IdClass 사용

--- src/main/java/com/nhnacademy/springbootjpa/entity/OrderItem.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/OrderItem.java
@@ -1,30 +1,25 @@
 package com.nhnacademy.springbootjpa.entity;
 
-// TODO #1: `order_item` 테이블과 매핑될 `OrderItem` Entity 클래스를 작성하세요. foreign key 부분은 무시합니다.
-/*
- * create table order_item
- * (
- *     order_id    bigint not null,
- *     line_number int    not null,
- *     item_id     bigint not null,
- *     quantity    int    not null,
- *     primary key (order_id, line_number),
- *     constraint order_item_item_id_fk
- *         foreign key (item_id) references item (id),
- *     constraint order_item_order_id_fk
- *         foreign key (order_id) references "order" (id)
- * );
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@IdClass(OrderItemPk.class)
+@Entity
 public class OrderItem {
-    public OrderItemPk getPk() {
-        return null;
-    }
+    @Id
+    private long orderId;
 
-    public long getItemId() {
-        return 0;
-    }
+    @Id
+    private int lineNumber;
 
-    public int getQuantity() {
-        return 0;
-    }
+    private long itemId;
+    private int quantity;
 }
