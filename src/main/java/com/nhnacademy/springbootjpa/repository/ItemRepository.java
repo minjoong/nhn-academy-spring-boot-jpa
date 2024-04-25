cc6adfd08090a16c0a8f7fa00515f5012023b520
김민중 <minjoong.slow@gmail.com>
2024-04-25 22:49:55 +0900

    `@Query` 데모

--- src/main/java/com/nhnacademy/springbootjpa/repository/ItemRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/ItemRepository.java
@@ -2,6 +2,37 @@ package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findAllByPriceGreaterThanEqual(long price);
+
+    // TODO #1: `@Query`
+    @Query("select i from Item i where i.price >= ?1")
+    List<Item> findAllByPriceAtLeastByQuery(long price);
+
+    // TODO #2: `@Query` with named parameter
+    @Query("select i from Item i where i.price >= :price")
+    List<Item> findAllByPriceAtLeastByQueryParam(long price);
+
+    // TODO #3: `@Query` with native query
+    @Query(value = "select * from item where price >= ?1", nativeQuery = true)
+    List<Item> findAllByPriceAtLeastByNativeQuery(long price);
+
+    // TODO #4: `@Query` with native query and named parameter
+    @Query(value = "select * from item where price >= :price", nativeQuery = true)
+    List<Item> findAllByPriceAtLeastByNativeQueryParam(long price);
+
+    // TODO #5: `@Modifying`
+    @Modifying
+    @Query("update Item i set i.name = :name where i.id = :id")
+    int updateNameByIdByQuery(long id, String name);
+
+    // TODO #6: `@Modifying` with `clearAutomatically`
+    @Modifying(clearAutomatically = true)
+    @Query("update Item i set i.name = :name where i.id = :id")
+    int updateNameByIdByQueryAndClear(long id, String name);
 }
--- src/test/java/com/nhnacademy/springbootjpa/repository/ItemRepositoryTest.java
+++ src/test/java/com/nhnacademy/springbootjpa/repository/ItemRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Item;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +18,9 @@ class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Sql("item-test.sql")
     @Test
@@ -28,6 +36,99 @@ class ItemRepositoryTest {
         assertThat(item.getId()).isEqualTo(itemId);
         assertThat(item.getName()).isEqualTo("apple");
         assertThat(item.getPrice()).isEqualTo(300L);
+    }
+
+    // TODO #7: test case (@Query를 사용한 조회 메소드)
+    @DisplayName("@Query를 사용한 조회 메소드 테스트")
+    @Sql("item-test.sql")
+    @Test
+    void queryTest() {
+        // JpaRepository 메소드 이름 규칙을 사용한 조회 메소드
+        List<Item> items1 = itemRepository.findAllByPriceGreaterThanEqual(250L);
+        assertThat(items1).hasSize(7);
+
+        // @Query 어노테이션을 사용한 조회 메소드
+        List<Item> items2 = itemRepository.findAllByPriceAtLeastByQuery(250L);
+        assertThat(items2).hasSize(7);
+
+        // @Query 어노테이션을 사용한 조회 메소드 (Named Parameter)
+        List<Item> items3 = itemRepository.findAllByPriceAtLeastByQueryParam(250L);
+        assertThat(items3).hasSize(7);
+    }
+
+    // TODO #8: test case (Native Query를 사용한 조회 메소드)
+    @DisplayName("@Query와 Native Query를 사용한 조회 메소드 테스트")
+    @Sql("item-test.sql")
+    @Test
+    void nativeQueryTest() {
+        // JpaRepository 메소드 이름 규칙을 사용한 조회 메소드
+        List<Item> items1 = itemRepository.findAllByPriceGreaterThanEqual(250L);
+        assertThat(items1).hasSize(7);
+
+        // @Query 어노테이션과 nativeQuery 속성을 사용한 조회 메소드
+        List<Item> items2 = itemRepository.findAllByPriceAtLeastByNativeQuery(250L);
+        assertThat(items2).hasSize(7);
+
+        // @Query 어노테이션과 nativeQuery 속성을 사용한 조회 메소드 (Named Parameter)
+        List<Item> items3 = itemRepository.findAllByPriceAtLeastByNativeQueryParam(250L);
+        assertThat(items3).hasSize(7);
+    }
+
+    // TODO #9: test case (@Modifying을 사용한 업데이트 메소드)
+    @DisplayName("@Modifying을 사용한 업데이트 메소드 테스트")
+    @Sql("item-test.sql")
+    @Test
+    void updateQueryTest() {
+        // given
+        long itemId = 1L;
+
+        // when
+        Item item1 = itemRepository.findById(itemId).orElse(null);
+
+        // then - 변경 전 name = apple
+        assertThat(item1).isNotNull();
+        assertThat(item1.getName()).isEqualTo("apple");
+
+        // when - @Modifying @Query 로 name 변경, name = samsung
+        itemRepository.updateNameByIdByQuery(itemId, "samsung");
+
+        // then - 바로 조회하면, 변경된 name = samsung이 아닌 이전 name = apple이 조회됨, 영속성 컨텍스트에는 아까 조회한 item1이 남아있음
+        Item item2 = itemRepository.findById(itemId).orElse(null);
+        assertThat(item2).isNotNull();
+        assertThat(item2.getName()).isNotEqualTo("samsung");
+        assertThat(item2.getName()).isEqualTo("apple");
+
+        // when - 영속성 컨텍스트를 초기화하고 다시 조회하면, 변경된 name = samsung이 조회됨
+        entityManager.clear();
+        item2 = itemRepository.findById(itemId).orElse(null);
+
+        // then
+        assertThat(item2).isNotNull();
+        assertThat(item2.getName()).isEqualTo("samsung");
+    }
+
+    // TODO #10: test case (@Modifying을 사용한 업데이트 메소드, clearAutomatically = true)
+    @DisplayName("@Modifying을 사용한 업데이트 메소드 테스트, clearAutomatically = true")
+    @Sql("item-test.sql")
+    @Test
+    void updateQueryAndClearTest() {
+        // given
+        long itemId = 1L;
+
+        // when
+        Item item1 = itemRepository.findById(itemId).orElse(null);
+
+        // then - 변경 전, name = apple
+        assertThat(item1).isNotNull();
+        assertThat(item1.getName()).isEqualTo("apple");
+
+        // when - @Modifying(clearAutomatically = true) @Query 로 name 변경, name = samsung
+        itemRepository.updateNameByIdByQueryAndClear(itemId, "samsung");
+
+        // then - 바로 조회하면, 변경된 name = samsung이 조회됨, clearAutomatically 옵션에 의해 영속성 컨텍스트가 자동으로 초기화됨
+        Item item2 = itemRepository.findById(itemId).orElse(null);
+        assertThat(item2).isNotNull();
+        assertThat(item2.getName()).isEqualTo("samsung");
     }
 
 }
