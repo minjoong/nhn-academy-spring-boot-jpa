32ede5e08208ad61df8199d7399267674bc8c121
김민중 <minjoong.slow@gmail.com>
2024-05-08 22:26:05 +0900

    Pageable 실습 정답

--- src/main/java/com/nhnacademy/springbootjpa/controller/GetMemberController.java
+++ src/main/java/com/nhnacademy/springbootjpa/controller/GetMemberController.java
@@ -1,16 +1,21 @@
 package com.nhnacademy.springbootjpa.controller;
 
 import com.nhnacademy.springbootjpa.entity.MemberNameOnlyView;
+import com.nhnacademy.springbootjpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class GetMemberController {
 
-    // TODO #1: `GET /members?page=2&size=3` 와 같은 요청을 처리하도록 아래 코드를 완성하세요.
+    private final MemberRepository memberRepository;
+
     @GetMapping("/members")
-    public Page<MemberNameOnlyView> findAll() {
-        return null;
+    public Page<MemberNameOnlyView> findAll(Pageable pageable) {
+        return memberRepository.findAllBy(pageable);
     }
 }
--- src/main/java/com/nhnacademy/springbootjpa/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/MemberRepository.java
@@ -1,20 +1,11 @@
 package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Member;
-import com.nhnacademy.springbootjpa.entity.MemberGreetingView;
 import com.nhnacademy.springbootjpa.entity.MemberNameOnlyView;
-import com.nhnacademy.springbootjpa.entity.MemberView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    // TODO #2: 닫힌 프로젝션 인터페이스를 반환하는 쿼리 메소드
-    MemberNameOnlyView queryById(long id);
-
-    // TODO #5: 열린 프로젝션 인터페이스를 반환하는 쿼리 메소드
-    MemberGreetingView readById(long id);
-
-    // TODO #8: 중첩 구조를 갖는 프로젝션 인터페이스를 반환하는 쿼리 메소드
-    List<MemberView> findAllByName(String name);
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Page<MemberNameOnlyView> findAllBy(Pageable pageable);
 }
