3d4476802da99155609e9d4ca4bc7aa9ab995207
김민중 <minjoong.slow@gmail.com>
2024-05-08 21:36:41 +0900

    DomainClassConverter 예제

--- src/main/java/com/nhnacademy/springbootjpa/controller/GetUserController.java
+++ src/main/java/com/nhnacademy/springbootjpa/controller/GetUserController.java
@@ -1,21 +1,22 @@
 package com.nhnacademy.springbootjpa.controller;
 
 import com.nhnacademy.springbootjpa.entity.User;
-import com.nhnacademy.springbootjpa.service.UserService;
+import com.nhnacademy.springbootjpa.error.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @RestController
 public class GetUserController {
 
-    private final UserService userService;
-
+    // TODO #1: DomainClassConverter를 이용해서 path variable로 Entity를 받음
     @GetMapping("/users/{id}")
-    public User getById(@PathVariable("id") String id) {
-        return userService.getById(id);
+    public User getById(@PathVariable("id") Optional<User> optionalUser) {
+        return optionalUser.orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
 }
