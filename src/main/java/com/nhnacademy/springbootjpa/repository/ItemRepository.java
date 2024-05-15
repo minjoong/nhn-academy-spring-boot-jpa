081dfd1566b3fb33d72c5eb709cfc571178f1333
김민중 <minjoong.slow@gmail.com>
2024-05-15 16:55:30 +0900

    쿼리 메서드 생성 실습

--- src/main/java/com/nhnacademy/springbootjpa/repository/ItemRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/ItemRepository.java
@@ -3,5 +3,33 @@ package com.nhnacademy.springbootjpa.repository;
 import com.nhnacademy.springbootjpa.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    // select *
+    // from item
+    // where name like '{name}'
+    List<Item> findAllByNameLike(String name);
+
+    // select id
+    // from item
+    // where name = '{name}'
+    //   and price = {price}
+    // limit 1
+    boolean existsByNameAndPrice(String itemName, long price);
+
+    // select count(*)
+    // from item
+    // where name like '%{name}%'
+    int countByNameLike(String name);
+
+    // delete
+    // from item
+    // where price between {price1} and {price2}
+    void deleteAllByPriceBetween(long price1, long price2);
+
+    // TODO : #1 실습 - 아래 쿼리 결과가 나오도록 메서드를 추가하세요.
+    // select *
+    // from item
+    // where price in (...)
 }
--- src/test/java/com/nhnacademy/springbootjpa/repository/ItemRepositoryTest.java
+++ src/test/java/com/nhnacademy/springbootjpa/repository/ItemRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -16,18 +18,44 @@ class ItemRepositoryTest {
 
     @Sql("item-test.sql")
     @Test
-    void findItemTest() {
+    void findAllByNameLikeTest() {
         // given
-        long itemId = 1L;
+        String nameLike = "%ng%";
 
         // when
-        Item item = itemRepository.findById(itemId).orElse(null);
+        List<Item> items = itemRepository.findAllByNameLike(nameLike);
 
         // then
-        assertThat(item).isNotNull();
-        assertThat(item.getId()).isEqualTo(itemId);
-        assertThat(item.getName()).isEqualTo("apple");
-        assertThat(item.getPrice()).isEqualTo(300L);
+        assertThat(items).hasSize(2);
+    }
+
+    @Sql("item-test.sql")
+    @Test
+    void existsByNameAndPriceTest() {
+        // given
+        String name = "mango";
+        long price = 350L;
+
+        // when
+        boolean exists = itemRepository.existsByNameAndPrice(name, price);
+
+        // then
+        assertThat(exists).isTrue();
+    }
+
+    // TODO : #2 ItemRepository 에 추가한 메서드를 사용하여 테스트를 완성하세요.
+    @Sql("item-test.sql")
+    @Test
+    void question() {
+        // given
+        List<Long> prices = List.of(100L, 200L);
+
+        // when
+        // TODO: #2-1 itemRepository 에 생성한 메서드를 위의 prices 매개변수를 줘서 호출하세요.
+        List<Item> items = null;
+
+        // then
+        assertThat(items).hasSize(3);
     }
 
 }
