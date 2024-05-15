80adef402148234a2d070e78832175ddc5d9b681
김민중 <minjoong.slow@gmail.com>
2024-05-15 16:25:14 +0900

    JpaRepository 생성 실습

--- src/main/java/com/nhnacademy/springbootjpa/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.springbootjpa.repository;
-
-import com.nhnacademy.springbootjpa.entity.OrderItem;
-import com.nhnacademy.springbootjpa.entity.OrderItemPk;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
-}
