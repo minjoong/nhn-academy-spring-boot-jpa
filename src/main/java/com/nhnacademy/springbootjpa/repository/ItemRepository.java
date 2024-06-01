0ffd02d3cca9df2eb0f38e7c9f1c399ff35f4550
김민중 <minjoong.slow@gmail.com>
2024-06-01 17:30:26 +0900

    `@EntityGraph` 예제

--- src/main/java/com/nhnacademy/springbootjpa/repository/ItemRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/ItemRepository.java
@@ -1,7 +1,14 @@
 package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Item;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    // TODO #1: `@EntityGraph` 어노테이션을 사용하여 `orderItems`와 `orderItems.order`를 fetch join으로 가져오도록 설정
+    @EntityGraph(attributePaths = {"orderItems", "orderItems.order"})
+    List<Item> findAllBy();
 }
